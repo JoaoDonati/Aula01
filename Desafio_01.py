46a71973dd35e1c087365b7f277f9ef20819b469
donati <joao.donati@gmail.com>
2025-11-04 18:52:15 +0000

    teceiro commit

--- Desafio_01.py
+++ Desafio_01.py
@@ -1,12 +1,8 @@
+### desafio 01 - Aula 01
+
 nome=input("Digite o nome: ")
 salario=float(input("Digite o salario:"))
 percentualbonus=float(input("Digite o bonus:"))
 bonus_total= salario * percentualbonus / 100
-
-###########################
-print("Nome: ", nome)
-#print("Total do bonus: ", bonus_total) 
-#print("Salario com bonus: ", salario + bonus_total)    
-#############################
 
 print(f"Nome: {nome} tem o bonus de  {bonus_total} e salario total de {salario + bonus_total}")
